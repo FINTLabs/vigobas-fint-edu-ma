dc87057dec8db7abd308d49a5b34aabaab23d866
Bernt Johnsen <berntj@outlook.com>
2025-11-18 12:54:21 +0100

    Resolve merge confict: Keep Fix employee also private cand not added to CS

--- VigoBAS.FINT.Edu.cs
+++ VigoBAS.FINT.Edu.cs
@@ -1551,14 +1551,14 @@ namespace VigoBAS.FINT.Edu
 
                     var anchor = ImportedObjectsList[GetImportEntriesIndex].eduPerson.Anchor();
 
-                    if (ImportedObjectsList[GetImportEntriesIndex].eduPerson.AnsettelsesforholdSkole.Count > 0 || ImportedObjectsList[GetImportEntriesIndex].eduPerson.ElevforholdHovedkategori != "privatist" || importPurePrivateStudents)
+                    if (!String.IsNullOrEmpty(ImportedObjectsList[GetImportEntriesIndex].eduPerson.PersonalAnsattnummer) || ImportedObjectsList[GetImportEntriesIndex].eduPerson.ElevforholdHovedkategori != "privatist" || importPurePrivateStudents)
                     {
                         Logger.Log.DebugFormat("Trying to add eduPerson csentry with anchor {0}", anchor);
                         csentries.Add(ImportedObjectsList[GetImportEntriesIndex].eduPerson.GetCSEntryChange());
                     }
                     else
                     {
-                        Logger.Log.InfoFormat("Student {0} has 'privatist' as only student category and is filtered from import to CS", anchor);
+                        Logger.Log.DebugFormat("Student {0} has 'privatist' as only student category and is filtered from import to CS", anchor);
                     }
                 }
                 if (ImportedObjectsList[GetImportEntriesIndex].eduGroup != null)
